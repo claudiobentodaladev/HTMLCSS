f290af5d5c4dffededb035727827ebbac3f04fcf
claudiobentodala <programmer.claudio@gmail.com>
2025-04-03 20:58:43 +0100

    update android project
    external javascript

---
+++ android/scripts/script.js
@@ -0,0 +1,16 @@
+function mudouTamanho(){
+    if(window.innerWidth >= 768 ){
+        itens.style.display = 'flex'
+    } 
+    else{
+        itens.style.display = 'flex'
+    }
+}
+function clickMenu() {
+     if (itens.style.display == 'none') {
+         itens.style.display = 'flex'
+    } 
+    else{
+        itens.style.display = 'none'
+    }
+}
